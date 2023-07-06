3d8aa08bb50448065c973b552aac13ee679706f6
Gabriel Yuji Yasuda <gabrielyyasuda@gmail.com>
2023-07-06 08:32:30 -0300

    Seed category table

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,14 @@
+﻿using BulkyWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
