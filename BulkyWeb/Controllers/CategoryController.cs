1cbf9eb07f4bdd9346543da176aba491fed2a570
Gabriel Yuji Yasuda <gabrielyyasuda@gmail.com>
2023-07-06 20:03:59 -0300

    Db context configuration

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,13 +1,22 @@
 ﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context; //means DB
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
 
         public IActionResult Index()
         {
+            List<CategoryModel> objCategoryList = _context.Categories.ToList();
+
             return View();
         }
     }
